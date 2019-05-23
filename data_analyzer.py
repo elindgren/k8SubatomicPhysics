fixed scaling

data_analyzer.py
@@ -3,6 +3,7 @@ import numpy as np
 import scipy as sp
 import scipy.signal
 import matplotlib
+import collections
 import matplotlib.pyplot as plt
 
 
@@ -15,7 +16,7 @@ def parse_file_to_dict(file, labelkeys=None):
         for line in f:
             vals = line.split()
             for i, val in enumerate(vals):
-                d[labelkeys[i]].append(float(val))  # np append is much slower
+                d[labelkeys[i]].append(int(val))  # np append is much slower
             line_nbr += 1
         for key in d:
             d[key] = np.array(d[key])
@@ -24,21 +25,29 @@ def parse_file_to_dict(file, labelkeys=None):
 
 # specify file name
 compare_coin = False
-filename = "na22_coinc_90"
+filename = "th_coinc"
 # parse .txt-file to dict
 data = parse_file_to_dict(filename, ["E1", "E2", "T"])
 # Calibrate channel - calibration data from lab
 # ******* Det 1
 # Peak 1
-cal1_chan1 = 3160
+cal1_chan1 = 3300
 cal1_peak1 = 1173.237
 # Peak 2
-cal1_chan2 = 3590
+cal1_chan2 = 3750
 cal1_peak2 = 1332.501
 # calibration values
+# m1 = (cal1_peak2-cal1_peak1)/(1-cal1_chan2/cal1_chan1)
+# k1 = (cal1_peak1-m1)/cal1_chan1
+# print(m1)
+# print(k1)
 cal1_1val = cal1_peak1/cal1_chan1  # energy per channel number
 cal1_2val = cal1_peak2/cal1_chan2
 cal1 = (cal1_1val + cal1_2val)/2  # take the mean
+# counts = np.bincount(data["E1"])
+# print(np.argsort(counts))
+# print(collections.Counter(data["E1"]))
+
 # calibrate channel E1
 data["E1"] = data["E1"]*cal1
 
@@ -46,10 +55,10 @@ data["E1"] = data["E1"]*cal1
 
 # ******* Det 2
 # Peak 1
-cal2_chan1 = 3025
+cal2_chan1 = 3300
 cal2_peak1 = 1173.237
 # Peak 2
-cal2_chan2 = 3435
+cal2_chan2 = 3750
 cal2_peak2 = 1332.501
 # calibration values
 cal2_1val = cal2_peak1/cal2_chan1  # energy per channel number
@@ -61,15 +70,17 @@ data["E2"] = data["E2"]*cal2
 
 # Declare subplots
 if compare_coin:
-    fig, axs = plt.subplots(1, 2, sharey=True, figsize=(10, 6))  # Lower resolution later - this is very high
-    axs[0].set_ylim(0,500)
+    fig, axs = plt.subplots(1, 2, sharey=True, figsize=(11.5, 6))  # Lower resolution later - this is very high
+    #axs[0].set_xlim(0, 2500)
+    axs[0].set_ylim(0, 500)
     axs[0].set_ylabel("Antal")
+    #axs[1].set_xlim(0, 2500)
     axs[1].set_ylim(0,500)
     # *************** No coincidence
     # ******* Plot E1
     binsE1_nc = len(np.unique(data["E1"]))  # bins with no coincidence
     axs[0].hist(data["E1"], binsE1_nc, log=False)
-    axs[0].set_xlabel("Energi, i KeV \n Utan koincidens")
+    axs[0].set_xlabel("Energi, i keV \n Utan koincidens")
     # ******* Plot E2
     # binsE2_nc = len(np.unique(data["E2"]))
     # axs[0,1].hist(data["E2"], binsE2_nc)
@@ -89,7 +100,7 @@ if compare_coin:
     # ******* Plot E1
     binsE1_wc = len(np.unique(coin_data["E1"]))
     axs[1].hist(coin_data["E1"], binsE1_wc, log=False)
-    axs[1].set_xlabel("Energi, i KeV \n Med koincidens")
+    axs[1].set_xlabel("Energi, i keV \n Med koincidens")
     # ******* Plot E2
     # binsE2_wc = len(np.unique(coin_data["E2"]))
     # axs[1, 1].hist(coin_data["E2"], binsE2_wc)
@@ -101,12 +112,11 @@ if compare_coin:
         for idx, item in enumerate(([axs[i].title, axs[i].xaxis.label, axs[i].yaxis.label] +
                      axs[i].get_xticklabels() + axs[i].get_yticklabels())):
             if idx < 4:
-                item.set_fontsize(16)
+                item.set_fontsize(20)
             else:
-                item.set_fontsize(14)
+                item.set_fontsize(15)
 else:
     fig, axs = plt.subplots(1,1,figsize=(6, 6))
-    axs.set_ylim(0, 500)
     axs.set_ylabel("Antal")
     axs.set_ylim(0, 500)
     # # *************** With coincidence
@@ -122,14 +132,14 @@ else:
     # ******* Plot E1
     binsE1_wc = len(np.unique(coin_data["E1"]))
     axs.hist(coin_data["E1"], binsE1_wc, log=False)
-    axs.set_xlabel("Energi, i KeV \n Med koincidens")
+    axs.set_xlabel("Energi, i keV \n Med koincidens")
 
     for idx, item in enumerate(([axs.title, axs.xaxis.label, axs.yaxis.label] +
                                 axs.get_xticklabels() + axs.get_yticklabels())):
         if idx < 4:
-            item.set_fontsize(16)
+            item.set_fontsize(20)
         else:
-            item.set_fontsize(14)
+            item.set_fontsize(15)
 
 plt.savefig(filename+"_hist.png", format="png", bbox_inches='tight')
 plt.show()
@@ -137,13 +147,15 @@ plt.show()
 # Plot coincidence spectrum
 # Declare subplots
 if compare_coin:
-    fig, axs = plt.subplots(1, 2, sharey=True, figsize=(10, 6))  # Lower resolution later - this is very high
+    fig, axs = plt.subplots(1, 2, sharey=True, figsize=(11.5, 6))  # Lower resolution later - this is very high
     axs[0].set_ylabel("Antal")
     # *************** No coincidence
     # ******* Plot E1
     axs[0].scatter(data["E1"], data["E2"], s=0.001)
-    axs[0].set_xlabel("Detektor A, Energi i KeV \n Utan koincidens")
-    axs[0].set_ylabel("Detektor B, Energi i KeV")
+    axs[0].set_ylim(0, 2500)
+    axs[0].set_xlim(0, 2500)
+    axs[0].set_xlabel("Detektor A, Energi i keV \n Utan koincidens")
+    axs[0].set_ylabel("Detektor B, Energi i keV")
 
     # # *************** With coincidence
     # First apply coincidence - loop through data and remove points where T \neq 0
@@ -157,15 +169,17 @@ if compare_coin:
 
     # ******* Plot E1
     axs[1].scatter(coin_data["E1"], coin_data["E2"], s=0.001)
-    axs[1].set_xlabel("Detektor A, Energi i KeV \n Med koincidens")
-    axs[1].set_ylabel("Detektor B, Energi i KeV")
+    axs[1].set_ylim(0, 2500)
+    axs[1].set_xlim(0, 2500)
+    axs[1].set_xlabel("Detektor A, Energi i keV \n Med koincidens")
+    axs[1].set_ylabel("Detektor B, Energi i keV")
 
     # Increase font size etc.
     for i in range(len(axs)):
         for idx, item in enumerate(([axs[i].title, axs[i].xaxis.label, axs[i].yaxis.label] +
                      axs[i].get_xticklabels() + axs[i].get_yticklabels())):
             if idx < 4:
-                item.set_fontsize(16)
+                item.set_fontsize(20)
             else:
                 item.set_fontsize(14)
 else:
@@ -184,15 +198,15 @@ else:
 
     # ******* Plot E1
     axs.scatter(coin_data["E1"], coin_data["E2"], s=0.01)
-    axs.set_ylim(0,3000)
-    axs.set_xlim(0,3000)
-    axs.set_xlabel("Detektor A, Energi i KeV")
-    axs.set_ylabel("Detektor B, Energi i KeV")
+    axs.set_ylim(0, 2000)
+    axs.set_xlim(0, 2000)
+    axs.set_xlabel("Detektor A, Energi i keV")
+    axs.set_ylabel("Detektor B, Energi i keV")
 
     for idx, item in enumerate(([axs.title, axs.xaxis.label, axs.yaxis.label] +
                                 axs.get_xticklabels() + axs.get_yticklabels())):
         if idx < 4:
-            item.set_fontsize(16)
+            item.set_fontsize(20)
         else:
             item.set_fontsize(14)
 plt.savefig(filename+"_coin.png", format="png", bbox_inches='tight')